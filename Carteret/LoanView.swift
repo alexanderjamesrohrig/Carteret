05360cc8cb281544dd79ee19d4d3a51b4a8d299a
Alexander Rohrig <me@alexanderrohrig.com>
2024-01-30 14:52:46 -0500

    Creation of ConstantManager

---
+++ Carteret/LoanView.swift
@@ -0,0 +1,18 @@
+//
+//  LoanView.swift
+//  Carteret
+//
+//  Created by Alexander Rohrig on 1/30/24.
+//
+
+import SwiftUI
+
+struct LoanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoanView()
+}
---
+++ Carteret/Managers/ConstantManager.swift
@@ -0,0 +1,14 @@
+//
+//  ConstantManager.swift
+//  Carteret
+//
+//  Created by Alexander Rohrig on 1/30/24.
+//
+
+import Foundation
+
+class ConstantManager {
+    static let shared = ConstantManager()
+    
+    var 
+}
